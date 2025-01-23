062cfec6191153442666f763f6c76b7dd6fa6270
Muhammad <arabfysal@gmail.com>
2025-01-23 08:10:07 -0800

    font setting wip

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Layout from './features/layout/layout';
-import Inventory from './features/inventory';
+// import Inventory from './features/inventory';
 import Orders from './features/menu/order';
 
 const App = () => (
@@ -10,7 +10,7 @@ const App = () => (
       <Route index element={<div>hello</div>} />
       <Route path="profile" element={<div>profile</div>} />
       <Route path="settings" element={<div>settings</div>} />
-      <Route path="inventory" element={<Inventory />} />
+      {/* <Route path="inventory" element={<Inventory />} /> */}
       <Route path="menu" element={<Orders />} />
     </Route>
   </Routes>
